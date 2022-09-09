1c241b36a9e0ea77bf48e18802e37e508eb9ae14
Daniel-Oluwole <107513835+Daniel-Oluwole@users.noreply.github.com>
2022-09-09 02:32:13 +0100

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,22 +1,11 @@
-
-#include <stdio.h>
-
+#include <stdio.h>
 /**
+ *main - Entry point
  *
- *  *main - Entry point
- *
- *   *
- *
- *    *Return: retun 1 after program execution
- *
- *     */
-
+ *Return: retun 1 after program execution
+ */
 int main(void)
-
 {
-
-		printf("with proper grammar, but the outcome is a piece of art,\n");
-
-			return (0);
-
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
 }
