5eeaa45d779fbd252eaf8097d987b7155a529fe8
Daniel-Oluwole <107513835+Daniel-Oluwole@users.noreply.github.com>
2022-09-09 02:27:04 +0100

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,11 +5,12 @@
  * Return: Always 0 (Success)
  */
 int main(void){
-	sizeof(char);
-	sizeof(int);
-	sizeof(long int);
-	sizeof(long long);
-	sizeof(float);
+	printf("Size of a char:\n",sizeof(char);
+	printf("Size of a char:\n",sizeof(int);
+	printf("Size of a char:\n",sizeof(long int);
+	printf("Size of a char:\n",sizeof(long long);
+	printf("Size of a char:\n",sizeof(float);
+	
 
 
 	return (0);
