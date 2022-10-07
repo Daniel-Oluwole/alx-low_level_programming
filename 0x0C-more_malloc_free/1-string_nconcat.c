e7eba4be5bfc8428d5529c2d0ca65b2d4ce98675
daniel oluwole <danieloluwole51@gmail.com>
2022-10-07 16:42:32 -0700

    function that concatenates two strings.

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -16,42 +16,41 @@
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 
 {
-		char *ar;
-			unsigned int i = 0;
-				unsigned int j = 0;
+	char *ar;
+	unsigned int i = 0;
+	unsigned int j = 0;
 
-					unsigned int m;
+	unsigned int m;
 
-						unsigned int p;
+	unsigned int p;
 
-							unsigned int k = 0;
+	unsigned int k = 0;
 
-								unsigned int len;
+	unsigned int len;
 
 
 
-									if (s1 == NULL)
+	if (s1 == NULL)
 
-												s1 = "";
+		s1 = "";
 
-										if (s2 == NULL)
+	if (s2 == NULL)
 
-													s2 = "";
+		s2 = "";
 
-											while (s1[i])
+	while (s1[i])
 
-														i++;
-												while (s2[j])
-															j++;
-													if (j > n)
-																j = n;
-														len = i + j;
-															ar = malloc(sizeof(char) * (len + 1));
-																if (ar == NULL)
-																			return (NULL);																	for (p = 0; p < i; p++)
-																				ar[k++] = s1[p];
-																		for (m = 0; m < j; m++)
-																					ar[k++] = s2[m];
-
-																			ar[k] = '\0';																				return (ar);																				
+		i++;
+	while (s2[j])
+		j++;
+	if (j > n)
+		j = n;
+	len = i + j;
+	ar = malloc(sizeof(char) * (len + 1));
+	if (ar == NULL)
+		return (NULL);									for (p = 0; p < i; p++)
+			ar[k++] = s1[p];
+	for (m = 0; m < j; m++)
+		ar[k++] = s2[m];
+   		ar[k] = '\0';																				return (ar);																				
 }
